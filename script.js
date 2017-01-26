9c13f8d67c375f939d887ab0185f7e0f44551822
Felipe <felipe503@gmail.com>
2017-01-26 11:44:34 -0500

    Add files to upload "script.js"

---
+++ script.js
@@ -0,0 +1,84 @@
+var lat = '';
+var lng = '';
+$(document).ready(function(){
+	function addZero(i) {
+		if (i < 10) {
+			i = "0" + i;
+		}
+		return i;
+	}
+
+	function timeNow() {
+		var d = new Date();
+		var x = document.getElementById("demo");
+		var h = addZero(d.getHours());
+		var m = addZero(d.getMinutes());
+		var s = addZero(d.getSeconds());
+		return h + ":" + m + ":" + s;
+	}
+	function showPosition(pos)
+	{
+		$('.latitude').text(pos.coords.latitude);
+		$('.longitude').text(pos.coords.longitude);
+		lat = $('.latitude').text();
+		lng = $('.longitude').text();
+		var min = 1;
+		var _stops = '';
+		var time = new Date();
+		$.getJSON('data/stops.json',function(stops){
+			$.each(stops,function(){
+				if(this.stop_lat && this.stop_lon)
+				{
+					if( min > Math.sqrt( Math.pow((lat - this.stop_lat), 2) + Math.pow((lng - this.stop_lon), 2) ))
+					{
+						min = Math.sqrt( Math.pow((lat - this.stop_lat), 2) + Math.pow((lng - this.stop_lon), 2) );
+						//console.log(this.stop_name+', '+this.stop_id)
+						_stops = this;
+						//console.log(_stops);
+					}
+				}
+			});
+			
+			$('.closest_stop').text(_stops.stop_name+ ' ' +_stops.stop_id);
+			
+			$.getJSON( 'data/stop_times.json')
+			.done(function( json ) {
+				
+				$.each(json.results,function(){
+					//console.log(this.stop_id)
+					if(_stops.stop_id == this.stop_id)
+						console.log(this);
+				});
+			})
+			.fail(function( jqxhr, textStatus, error ) {
+				var err = textStatus + ", " + error;
+				console.log( "Request Failed: " + err );
+			});
+			/*
+			$.getJSON('data/stop_times.json',function(stop_times){
+				$.each(stop_times,function(){
+					$.each(this, function(){
+						if(this.stop_id == _this.stop_id)
+							console.log(this);
+					});
+					
+				});
+			});
+			*/
+		});
+	}
+	function initGeoLocation(){
+		if (navigator.geolocation) 
+		{
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} 
+		else 
+		{
+			console.log("Geolocation is not supported by this browser.");
+		}
+	}
+	$('.getCurrentLocation').click(function(e){
+		e.preventDefault();
+		initGeoLocation();
+	});
+});
